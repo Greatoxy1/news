c9b83fd99612775756b83b7822f7f568e8229c27
Greatoxy1 <156723421+Greatoxy1@users.noreply.github.com>
2026-06-07 07:25:05 +0200

    update news content

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import About from "./pages/About";
 import Stories from "./pages/Stories";
 import Event from "./pages/Event";
 import News from "./pages/News";
-import Article from "./pages/Article";
-import Footer from "./components/Footer";
 import BreakingNews from "./components/BreakingNews";
 import Subscribe from "./components/Subscribe";
 import World from "./components/World";
@@ -15,7 +13,6 @@ function App() {
     <>
       <Header />
       <BreakingNews />
-      <Article/>
       <World/>
       <Subscribe/>
       <Routes>
@@ -24,10 +21,8 @@ function App() {
         <Route path="/stories" element={<Stories />} />
         <Route path="/event" element={<Event />} />
         <Route path="/news" element={<News />} />
-        <Route path="/article" element={<Article />} />
       </Routes>
 
-      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <p style={{ fontSize: "12px" }}>
-        As an Amazon Associate I earn from qualifying purchases.
-      </p>
-
-    )
-}
-export default Footer;
